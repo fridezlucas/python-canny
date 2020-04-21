56453f6314c0aa0cdda4de48879e1546d6df3e1d
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-04-21 09:12:10 +0200

    Added main python module and  folder

---
+++ ts-canny-project.py
@@ -0,0 +1,10 @@
+""" TS Canny Project
+
+Project for TS lesson in Bsc 2
+
+Authors : Fridez Lucas, Goffinet Edouard, Laissue Luca
+Date    : 2020.04.21
+Version : 1.0
+"""
+
+print("ts-canny-project\nAuthors : Fridez Lucas, Goffinet Edouard, Laissue Luca\nVersion : 1.0\n\n")
