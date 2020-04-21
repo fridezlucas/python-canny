36f17d63e1b37db096d3f51cbf9124a2dd18b907
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-04-21 13:02:09 +0200

    Added file_selector module

---
+++ lib/file_selector.py
@@ -0,0 +1,47 @@
+""" File Selector module
+
+Project for TS lesson in Bsc 2
+
+Authors : Fridez Lucas, Goffinet Edouard, Laissue Luca
+Date    : 2020.04.21
+Version : 1.0
+"""
+
+import os
+
+
+def getAllFiles(dir):
+    """ Get all images files
+
+    Only keep images files (*.png *.jpg *.jpeg)
+    """
+
+    listFiles = []
+    for root, directories, files in os.walk(os.getcwd() + "/" + dir):
+        for file in files:
+            listFiles.append(os.path.join(root, file))
+
+    return listFiles
+
+
+def writeAllFiles(files):
+    """ Write a list of file with index
+    """
+
+    i = 0
+    for f in files:
+        print(f"[{i}] : {f}")
+        i += 1
+
+
+def chooseAnImage(dir):
+    """ Allow users to choose an image from a list
+
+    dir : directory in which user will choose image
+    """
+    files = getAllFiles(dir)
+    writeAllFiles(files)
+
+    indexImage = (int)(input("Which img would you like to analyse : "))
+
+    print(f"Image chosen : {files[indexImage]}")
--- ts-canny-project.py
+++ ts-canny-project.py
@@ -8,3 +8,12 @@ Version : 1.0
 """
 
 print("ts-canny-project\nAuthors : Fridez Lucas, Goffinet Edouard, Laissue Luca\nVersion : 1.0\n\n")
+
+# Import All modules
+import lib.file_selector as file_selector
+
+def main():
+    file_selector.chooseAnImage("img")
+
+if(__name__ == "__main__"):
+    main()
