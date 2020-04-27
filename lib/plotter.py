2e305b3f0bc954d77907b6153c9e539189e34a93
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-04-27 16:10:06 +0200

    Added CMYK analyze

--- lib/plotter.py
+++ lib/plotter.py
@@ -10,9 +10,10 @@ Version : 1.0
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from PIL import Image
 
 
-def erase2RGBValues(image, v1, v2):
+def erase2Values(image, v1, v2):
     """ Erase 2 RGB values (set to 0)
 
     v1 : index value 1 to reset
@@ -34,7 +35,7 @@ def erase2RGBValues(image, v1, v2):
 def getBlackWhiteImage(image):
     """ Get an image in grayscale
 
-    image  : Image to convert to grayscale
+    image  : Image to convert to grayscale  
     return : new image in grayscale
     Source : https://kite.com/python/answers/how-to-convert-an-image-from-rgb-to-grayscale-in-python
     """
@@ -76,17 +77,40 @@ def plotImageRGB(image):
     axes[0, 0].imshow(img)
 
     axes[0, 1].set_title("Image R values")
-    axes[0, 1].imshow(erase2RGBValues(img, 1, 2))
+    axes[0, 1].imshow(erase2Values(img, 1, 2))
 
     axes[1, 0].set_title("Image G values")
-    axes[1, 0].imshow(erase2RGBValues(img, 0, 2))
+    axes[1, 0].imshow(erase2Values(img, 0, 2))
 
     axes[1, 1].set_title("Image B values")
-    axes[1, 1].imshow(erase2RGBValues(img, 0, 1))
+    axes[1, 1].imshow(erase2Values(img, 0, 1))
 
     figure.tight_layout()
     plt.show()
 
+def plotImageCMY(image):
+    figure, axes = plt.subplots(nrows=2, ncols=2)
+    figure.canvas.set_window_title("Plot 2/3 : Image CMY")
+
+    img = Image.open(image)
+
+    plt.suptitle("Analyse RGB image")
+    axes[0, 0].set_title("Original image")
+    axes[0, 0].imshow(img)
+
+    img = np.asarray(Image.open(image).convert("CMYK"))
+
+    axes[0, 1].set_title("Image C values")
+    axes[0, 1].imshow(Image.fromarray(np.uint8(erase2Values(img, 1, 2)), mode="CMYK"))
+
+    axes[1, 0].set_title("Image M values")
+    axes[1, 0].imshow(Image.fromarray(np.uint8(erase2Values(img, 0, 2)), mode="CMYK"))
+
+    axes[1, 1].set_title("Image Y values")
+    axes[1, 1].imshow(Image.fromarray(np.uint8(erase2Values(img, 0, 1)), mode="CMYK"))
+
+    figure.tight_layout()
+    plt.show()
 
 def plotAll(image):
     """ Plot all variants with image given
@@ -94,4 +118,5 @@ def plotAll(image):
     image : image to plot in multiple variants
     """
     plotImageRGB(image)
+    plotImageCMY(image)
     plotGrayscaleImage(image)
